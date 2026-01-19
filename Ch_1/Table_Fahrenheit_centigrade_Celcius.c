a8373f023c0cd210461f181b54780b72df01805c
WearyTravler <limbo_shell@protonmail.com>
2026-01-19 17:48:19 -0600

    the changes that display diff format temp values

--- Ch_1/Table_Fahrenheit_centigrade_Celcius.c
+++ Ch_1/Table_Fahrenheit_centigrade_Celcius.c
@@ -5,7 +5,7 @@
 
 main()
 {
-    int fahr, celcius;
+    float fahr, celcius;
     int lower, upper, step;
 
     lower = 0; /* lower limit of temperature table */
@@ -14,8 +14,8 @@ main()
 
     fahr = lower;
     while (fahr <= upper) {
-        celcius = 5* (fahr-32)/9;
-        printf("%d\t%d\n",fahr,celcius);
+        celcius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n",fahr,celcius); // makes it 'justified'
         fahr = fahr + step;
     }
 
